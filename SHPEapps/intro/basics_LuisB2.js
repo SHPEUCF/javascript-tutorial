dfff400097434b4afd18f94594efa65923ab33b5
Luis Benavides <luisbenan@gmail.com>
2017-10-05 20:57:08 -0400

    Add tutorial file from meeting

---
+++ SHPEapps/intro/basics_LuisB2.js
@@ -0,0 +1,16 @@
+// Single comments
+
+/*
+This is a
+multi-line comment
+*/
+
+console.log('Cheese');
+console.log('Peaceful Warrior');
+
+// Data types
+
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
