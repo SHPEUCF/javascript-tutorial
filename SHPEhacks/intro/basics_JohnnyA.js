62012816d2e38aa27f88bcd39e36860551924c88
Johnny Arcila <johnnyar910@hotmail.com>
2017-10-06 19:52:01 -0400

    add tutorial file

--- SHPEhacks/intro/basics_JohnnyA.js
+++ SHPEhacks/intro/basics_JohnnyA.js
@@ -12,4 +12,4 @@ var checkingaccount  = 1000000;
 let myName  = "Johnny";
 const year = 2017;
 
-console.log("My  account balance: ' + checkingaccount");
+console.log("My  account balance: "  + checkingaccount);
