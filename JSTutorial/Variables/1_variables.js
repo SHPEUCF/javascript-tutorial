266cd2448e390ca43e876551d02d675aa6ad3df2
Luis Benavides <luisbenan@gmail.com>
2017-10-16 09:57:18 -0400

    Add Variables lesson files

---
+++ JSTutorial/Variables/1_variables.js
@@ -0,0 +1,40 @@
+/*
+1. VARIABLES
+
+Variables allow us to assign data to a word
+and use the word to reference the data.
+If the data changes we can replace the
+variable's value instead of re-writing
+the program.
+
+Two ways to declare variables in JavaScript:
+let and const
+
+*/
+
+
+// --- On Codecademy ---
+
+/*
+
+ 1. Run your code, what was printed?
+ 2. Add the following to the bottom of App.js
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+ */
+
+// Example
+
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
---
+++ JSTutorial/Variables/4_undefined.js
@@ -0,0 +1,31 @@
+/*
+1. UNDEFINED
+
+JavaScript creates space for this variable
+in memory and sets it to undefined
+
+Undefined is the fifth and final primitive
+data type.
+
+let whatAmI;
+
+
+*/
+
+
+// --- On Codecademy ---
+
+/*
+
+ 1. Create an undefined variable named
+notDefined
+
+ 2. Create an undefined variable named
+ valueless
+
+ */
+
+// Example
+
+let notDefined;
+let valueless;
