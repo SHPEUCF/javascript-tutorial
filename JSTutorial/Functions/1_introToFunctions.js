51d606ea34e716b7681f45207298419f206ba5a3
Luis Benavides <luisbenan@gmail.com>
2017-10-17 22:23:03 -0400

    Add functions lesson files

---
+++ JSTutorial/Functions/1_introToFunctions.js
@@ -0,0 +1,22 @@
+/*
+1. INTRO TO FUNCTIONS
+
+A block of code designed to perform a task.
+
+They accept data, perform actions on that data,
+and return a result.
+
+We can reuse it over and over without rewriting
+the same code.
+
+*/
+
+
+// --- On Codecademy ---
+
+/*
+
+ 1. Try to understand the funciton
+ then move on! NEXT
+
+ */
