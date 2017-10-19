5fac6a4cf853d7a6d9e0e5995835b4b904822dc8
Luis Benavides <luisbenan@gmail.com>
2017-10-19 12:37:38 -0400

    Add scope files

--- JSTutorial/Scope/3_globalScopeII.js
+++ JSTutorial/Scope/3_globalScopeII.js
@@ -1,5 +1,13 @@
 /*
-1. NAME
+3. GLOBAL SCOPE II
+
+While it's important to know what global scope is,
+it's better to avoid defining variables in the
+global scope. Globally scoped variables can collide
+ with variables that are more locally scoped,
+ causing unexpected behavior in our code.
+
+
 
 
 
@@ -10,8 +18,28 @@
 
 /*
 
- 1.
+ 1. change stars = 'Sirius'; on first line of
+  myNightSky
+
+ 2. console.log(stars);
+
+ Lesson: it changes global value,
+ maybe unintentionally
+
 
  */
 
 // Example
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+
+let stars = 'North Star';
+
+const myNightSky = () => {
+  stars = 'Sirius';
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+}
+
+console.log(myNightSky());
+console.log(stars);
