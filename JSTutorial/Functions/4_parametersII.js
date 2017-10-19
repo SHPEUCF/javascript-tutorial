bca949d5d93eb35723810d0402d47a35dcedb0f0
Luis Benavides <luisbenan@gmail.com>
2017-10-18 23:58:59 -0400

    Add functions files

--- JSTutorial/Functions/4_parametersII.js
+++ JSTutorial/Functions/4_parametersII.js
@@ -1,19 +1,45 @@
 /*
-1. TITLE
+4. PARAMETERS II
 
-
+We can set as many parameters as we'd like
+by adding them when we declare the function,
+separated by commas,
 
 */
+
+const getAverage = (numberOne, numberTwo) => {
+  const average = (numberOne + numberTwo) / 2 ;
+  console.log(average);
+};
+
+getAverage(365, 27);
+// Output: 196
+
 
 
 // --- On Codecademy ---
 
 /*
 
- 1.
+ 1. Add crustType parameter to takeOrder()
 
- 2.
+ 2. interpolate the crustType parameter to log
+ Order: thin crust pizza topped with bacon
+
+ 3. call takeOrder() three times and do this:
+ takeOrder('ham', 'thin');
+ takeOrder('pineapple', 'regular');
+ takeOrder('mushrooms', 'crunchy');
+
 
  */
 
 // Example
+
+const takeOrder = (topping, crustType) => {
+  console.log(`Order: ${crustType} crust pizza topped with ${topping}`);
+};
+
+takeOrder('ham', 'thin');
+takeOrder('pineapple', 'regular');
+takeOrder('mushrooms', 'crunchy');
--- JSTutorial/Functions/5_return.js
+++ JSTutorial/Functions/5_return.js
@@ -1,19 +1,59 @@
 /*
-1. TITLE
+5. RETURN
 
+The purpose of a function is to take some input,
+perform some task on that input, then return a
+result.
 
+To return a result, we can use the return keyword.
 
 */
+
+const getAverage = (numberOne, numberTwo) => {
+  const average = (numberOne + numberTwo) / 2;
+  return average;
+}
+
+console.log(getAverage(365, 27));
+// Output: 14
+
 
 
 // --- On Codecademy ---
 
 /*
 
- 1.
+1. create orderCount = 0 at top of program
 
- 2.
+2. inside takeOrder() increase orderCount by 1
+orderCount++;
+
+3. declare a new function named getSubTotal
+that has one parameter named itemCount.
+
+4. Inside the getSubTotal() function's block,
+use return to output the itemCount
+multiplied by 7.5
+
+5. call getSubTotal() pass, orderCount, console.log
+console.log(getSubTotal(orderCount));
 
  */
 
 // Example
+
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log(`Order: ${crustType} crust pizza topped with ${topping}`);
+};
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+}
+
+takeOrder('ham', 'thin');
+takeOrder('pineapple', 'regular');
+takeOrder('mushrooms', 'crunchy');
+console.log(getSubTotal(orderCount));
