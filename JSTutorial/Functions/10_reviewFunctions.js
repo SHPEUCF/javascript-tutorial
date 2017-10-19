7ad835c10d6b2b79d794ca4f2417b032796ddf46
Luis Benavides <luisbenan@gmail.com>
2017-10-19 00:22:48 -0400

    Add final functions files

--- JSTutorial/Functions/10_reviewFunctions.js
+++ JSTutorial/Functions/10_reviewFunctions.js
@@ -1,7 +1,21 @@
 /*
-1. TITLE
+10. SUMMARY OF Functions
 
 
+- Functions are written to perform a task.
+
+- Functions take data, perform a set of tasks on
+the data, and then return the result.
+
+- We can define parameters to be used when calling
+the function.
+
+- When calling a function, we can pass in arguments,
+ which will set the function's parameters.
+
+- We can use return to return the result of a
+function which allows us to call functions anywhere,
+even inside other functions.
 
 */
 
@@ -10,10 +24,6 @@
 
 /*
 
- 1.
+UP NEXT
 
- 2.
-
- */
-
-// Example
+*/
--- JSTutorial/Functions/7_functionDeclarations.js
+++ JSTutorial/Functions/7_functionDeclarations.js
@@ -1,19 +1,40 @@
 /*
-1. TITLE
+7. FUNCTION DECLARATION
 
+function square (number) {
+  return number * number;
+}
 
+keyword, name, = {}
+
+Arrow functions later
 
 */
-
 
 // --- On Codecademy ---
 
 /*
 
- 1.
+ 1. declare function isGreaterThan,
+ parameters: numberOne, numberTwo
 
- 2.
+ 2. inside, use if/else:
+ if numberOne > numberTwo return true
+ else return false
+
+ 3. call the isGreaterThan() with two
+ arguments, any two numbers
 
  */
 
 // Example
+
+function isGreaterThan (numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+isGreaterThan(5, 6);
